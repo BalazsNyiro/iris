739f16543dea4ed17dbfe806cf710abf26e3ff16
Balazs Nyiro <diogenesz@pergamen.hu>
2022-10-20 07:02:47 -0400

    terminal dimensions detect steps

--- iris/iris.go
+++ iris/iris.go
@@ -5,6 +5,7 @@ import (
     "bytes"
     "fmt"
     "log"
+    "os"
     "os/exec"
     "strings"
 )
@@ -16,19 +17,26 @@ func Page() string {
 }
 
 // https://stackoverflow.com/questions/263890/how-do-i-find-the-width-height-of-a-terminal-window
+// https://stackoverflow.com/questions/16569433/get-terminal-size-in-go
 func terminal_detect() (int, int) {
-    out, _ := shell("ls -la")
-    out, _ = shell("stty size")
-    fmt.Println(">>>", out)
+    out, _ := shell("stty size")
+    fmt.Println("terminal detect >>>", out)
     return 1, 2
 }
 
 // https://zetcode.com/golang/exec-command/
 func shell(commandAndParams string) (string, error) {
+    return shellCore(commandAndParams, "")
+}
+
+func shellCore(commandAndParams, input string) (string, error) {
     args := strings.Fields(commandAndParams)
-    fmt.Println("args:", args)
     cmd := exec.Command(args[0], args[1:]...)
-    cmd.Stdin = strings.NewReader("some input")
+    if len(input) > 0 {
+        cmd.Stdin = strings.NewReader(input)
+    } else {
+        cmd.Stdin = os.Stdin
+    }
     var out bytes.Buffer
     cmd.Stdout = &out
     err := cmd.Run()
