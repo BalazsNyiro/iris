25730e7a1c23676a1c4c124577d9cbe621dbd81e
Balazs Nyiro <diogenesz@pergamen.hu>
2022-10-20 20:42:16 -0400

    git

---
+++ iris/example_projects/iris_windows.go
@@ -0,0 +1,12 @@
+package main
+
+// run: go run iris_windows.go
+
+import "fmt"
+import "github.com/BalazsNyiro/iris/iris"
+
+// run: go run jyp_example_usage.go
+
+func main() {
+    fmt.Println(iris.TerminalDetect())
+}
