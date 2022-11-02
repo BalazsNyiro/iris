56c46ce9aa9a2b6b04f5846ce8f90fc1ac8e12b8
Balazs Nyiro <diogenesz@pergamen.hu>
2022-11-01 22:11:27 -0400

    test: calculated width

--- iris/iris.go
+++ iris/iris.go
@@ -1,6 +1,11 @@
 // author: Balazs Nyiro, balazs.nyiro.ca@gmail.com
 package iris
 
+import (
+	"strconv"
+	"strings"
+)
+
 /*
 	all DOM objects are containers
     An object can have childrens (other objects)
@@ -12,20 +17,61 @@ package iris
 
 */
 type DomObj struct {
-    /*
-       in a gui we can use 'px'. here we use: char as base width unit
-    */
-    Attr     map[string]string
-    Children []DomObj
-    Parent   *DomObj
+	/*
+		in a gui we can use 'px'. here we use: char as base width unit
+
+		Area: A DomObj has an area in the screen.
+		=========================================
+		All children is created from one coordinate point:
+		        the object tries to form the wanted fix sizes (fix width or height)
+		        or a relative size: (height_available-1) for example
+
+	*/
+	Attr     map[string]string
+	Children []DomObj
+	Parent   *DomObj
 }
 
-func ObjNew(id, width, height string) DomObj {
-    attr := map[string]string{"id": id, "width": width, "height": height}
-    return DomObj{Attr: attr}
+func ObjNew(id, width, height, bornX, bornY string, parentPointer *DomObj) DomObj {
+	attr := map[string]string{
+		"id": id, // all id is unique. Equivalent with dom Id
+
+		"width":             width,             // value examples: 20, 20%,
+		"height":            height,            // or it can be empty: ''
+		"bornX":             bornX,             // default unit: character.
+		"bornY":             bornY,             // 20 means: 20 character
+		"bornHorizontalWay": "FromLeftToRight", // or FromRightToLeft
+		"bornVerticalWay":   "FromTopToDown",   // or FromDownToTop
+	}
+	return DomObj{Attr: attr, Parent: parentPointer}
+}
+func (obj DomObj) WidthCalculated() int {
+	return valueStringToNumber(obj.Attr["width"], obj.Parent, "width")
 }
 
-func DocumentCreate(id, width, height string) DomObj {
-    root := ObjNew(id, width, height)
-    return root
+func valueStringToNumber(valStr string, parentPtr *DomObj, parentAttrKey string) int {
+	valStr = strings.TrimSpace(valStr)
+	valCalculated := 0
+	if strings.Contains(valStr, "%") {
+		valStr = strings.Replace(valStr, "%", "", -1)
+		val, err := strconv.Atoi(valStr)
+		if err == nil && parentPtr != nil {
+			valCalculated = valueStringToNumber(
+				parentPtr.Attr[parentAttrKey],
+				parentPtr.Parent,
+				parentAttrKey) * val / 100
+		}
+	} else {
+		val, err := strconv.Atoi(valStr)
+		if err == nil {
+			valCalculated = val
+		}
+	}
+	return valCalculated
+}
+
+func DocumentCreate(id, width, height, terminalWidth, terminalHeight string) DomObj {
+	parentTerminal := ObjNew("terminal", terminalWidth, terminalHeight, "", "", nil)
+	root := ObjNew("documentRoot", width, height, "0", "0", &parentTerminal)
+	return root
 }
--- iris/iris_test.go
+++ iris/iris_test.go
@@ -1,15 +1,32 @@
 package iris
 
+import "testing"
+
 // author: Balazs Nyiro, balazs.nyiro.ca@gmail.com
 
-import (
-	"fmt"
-	"testing"
-)
-
+/*
 func Test_terminal_detect(t *testing.T) {
 	widthStty, heightStty := TerminalDimensionsSttySize()
 	fmt.Println("test stty:", widthStty, heightStty)
 	widthSys, heightSys := TerminalDimensionsWithSyscall()
 	fmt.Println("test syscall:", widthSys, heightSys)
+}
+func Test_document_create(t *testing.T) {
+	rootObj := DocumentCreate("0", "50%", "50%", "40", "20")
+
+}
+
+*/
+func Test_document_create(t *testing.T) {
+	// rootObj := DocumentCreate("0", "50%", "50%", "40", "20")
+
+	// no parent object usage, not a relative % value:
+	value := valueStringToNumber("20", nil, "")
+	compare_int_pair(value, 20, t)
+}
+
+func compare_int_pair(received, wanted int, t *testing.T) {
+	if received != wanted {
+		t.Fatalf("\nreceived: %v\n  wanted: %v, error", received, wanted)
+	}
 }
