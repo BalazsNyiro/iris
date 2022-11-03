4906e641d84dbfe60e1c2d21a46cb5a4cb70bb58
Balazs Nyiro <diogenesz@pergamen.hu>
2022-11-02 20:30:51 -0400

    tests

--- iris/iris_test.go
+++ iris/iris_test.go
@@ -1,6 +1,8 @@
 package iris
 
-import "testing"
+import (
+	"testing"
+)
 
 // author: Balazs Nyiro, balazs.nyiro.ca@gmail.com
 
@@ -17,12 +19,20 @@ func Test_document_create(t *testing.T) {
 }
 
 */
-func Test_document_create(t *testing.T) {
-	// rootObj := DocumentCreate("0", "50%", "50%", "40", "20")
+func Test_value_calculations(t *testing.T) {
+	//rootObj := DocumentCreate("0", "50%", "50%", "60", "20")
 
 	// no parent object usage, not a relative % value:
 	value := valueStringToNumber("20", nil, "")
 	compare_int_pair(value, 20, t)
+
+	rootObj := DocumentCreate("0", "60", "20", "", "0")
+	valuePercent := valueStringToNumber("20%", &rootObj, "width")
+	compare_int_pair(valuePercent, 12, t)
+
+	rootObjHalfTerminal := DocumentCreate("0", "50%", "50%", "50", "20")
+	valuePercentInHalfRoot := valueStringToNumber("20%", &rootObjHalfTerminal, "width")
+	compare_int_pair(valuePercentInHalfRoot, 5, t)
 }
 
 func compare_int_pair(received, wanted int, t *testing.T) {
