49c9ecbd5b6c06030d110760ed5a9a5de5f94b71
Balazs Nyiro <diogenesz@pergamen.hu>
2023-07-11 21:33:50 -0400

    manual_test

--- iris/manual_test/manual_test_iris_windows_run.sh
+++ iris/manual_test/manual_test_iris_windows_run.sh
@@ -1,8 +1,9 @@
 # manual local test 
 
 cp ../iris*.go .
-rm *test*.go
+rm iris*test*.go
 sed --in-place "s/package iris/package main/g" iris*.go
 go run manual_test_iris_windows.go iris*.go
 rm iris*.go
 
+reset
