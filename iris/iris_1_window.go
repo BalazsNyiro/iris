488d553f1ca4fb39cd46f0f7619962e2e13b89cf
Balazs Nyiro <diogenesz@pergamen.hu>
2023-09-07 22:04:36 -0400

    windows render

--- iris/iris_1_window.go
+++ iris/iris_1_window.go
@@ -16,6 +16,9 @@ package iris
 
 import "fmt"
 
+type ColumnChars []Char
+type MatrixChars []ColumnChars // 2 dimensional Char structure
+
 type Window struct {
 	id string
 
@@ -24,14 +27,79 @@ type Window struct {
 	xLeft             int
 	width             int
 	height            int
-	lines             []LineChars
+	textBlockPtr      *TextBlock
 	backgroundDefault string
-	winId             string
+	winIdNum          int
+	winHumanName      string
 }
 
 func (w Window) print() {
-	fmt.Println("winId:  ", w.winId)
-	for _, line := range w.lines {
+	fmt.Println("winId:  ", w.winIdNum)
+
+	fmt.Println("textBlock lineNum", len(w.textBlockPtr.Lines))
+	for _, line := range w.textBlockPtr.Lines {
 		fmt.Println("winLine:", line.LineToStr())
 	}
+}
+func (w Window) render() {
+	// TODO: matrix := Matrix_empty()
+
+	/* How can we render? :-) This is a game!
+
+	an example textBlock.Lines (2 lines:)
+	abcdefghijklm
+	ABCDEFGHIJKLMN
+
+	How can we render this? the window width = 4, height = 5
+
+	A window map:
+	  1234   the last line needs to be broken to win.width sections:
+	1 ....
+	2 ....      ABCD  <- totally filled FIRST window row
+	3 ....	    EFGH  <- totally filled       window row
+	4 ....	    IJKL  <- totally filled       window row
+	5 ....	    MN    <- partial              window row
+
+	one text line can fille more window row.
+
+	so the textBlock.Lines needs to be splitted up smaller
+	sections, and in reverse orders we need to upload the lines.
+	*/
+
+	fmt.Println("RENDER")
+	fmt.Println("textBlock lineNum", len(w.textBlockPtr.Lines))
+	for i := len(w.textBlockPtr.Lines) - 1; i >= 0; i-- {
+		line := w.textBlockPtr.Lines[i]
+		fmt.Println("winLine:", i, line.LineToStr())
+	}
+}
+
+func window_row_start_end_positions(lengtOfTextBlockLine int, windowWidth int) [][2]int {
+
+	positionValues := [][2]int{}
+	lengthActual := lengtOfTextBlockLine
+
+	for true {
+		positionEnd := lengthActual - 1
+		positionStart := positionEnd // a default value only, overwritten in if{}
+		partialNotFullWindowRow_text_length := lengthActual % windowWidth
+
+		if partialNotFullWindowRow_text_length > 0 {
+			positionStart = positionEnd - partialNotFullWindowRow_text_length + 1
+		} else {
+			positionStart = positionEnd - windowWidth + 1
+		}
+		/*Concrete example: position 13 and position 14 are the partials.
+		so we need to select (13, 14) first. but the length will be
+		decreased with 2:  14-13+1 = 2
+		*/
+		lengthActual = lengthActual - (positionEnd - positionStart + 1)
+
+		positionValues = append(positionValues, [2]int{positionStart, positionEnd})
+		if positionStart == 0 {
+			break
+		}
+	}
+
+	return positionValues
 }
--- iris/iris_test.go
+++ iris/iris_test.go
@@ -41,16 +41,48 @@ func Test_text_create(t *testing.T) {
 
 func Test_text_append(t *testing.T) {
 	funName := "Test_text_append"
-	text := TextBlockFromStr("a\nbc\ndef")
-	TextAppendIntoLastLine("ghi", &text)
+	textBlock := TextBlockFromStr("a\nbc\ndef")
+	TextAppendIntoLastLine("ghi", &textBlock)
 
-	compare_int_pair(funName, len(text.Lines), 3, t)
-	compare_int_pair(funName, len(text.Lines[2].Chars), 6, t)
+	compare_int_pair(funName, len(textBlock.Lines), 3, t)
+	compare_int_pair(funName, len(textBlock.Lines[2].Chars), 6, t)
 
-	TextAppendIntoNewNextLine("jklmn", &text)
-	compare_int_pair(funName, len(text.Lines), 4, t)
-	compare_int_pair(funName, len(text.Lines[3].Chars), 5, t)
+	TextAppendIntoNewNextLine("jklmn", &textBlock)
+	compare_int_pair(funName, len(textBlock.Lines), 4, t)
+	compare_int_pair(funName, len(textBlock.Lines[3].Chars), 5, t)
 
+}
+
+func Test_windows_render(t *testing.T) {
+	//funName := " Test_windows_display"
+	textBlock := TextBlockFromStr("1a\n2bc\n3def\n4ghijklmno")
+	winId := 0
+
+	win := Window{yTop: 0, xLeft: 0, width: 4, height: 3, textBlockPtr: &textBlock, backgroundDefault: "b", winIdNum: winId}
+	win.print()
+
+	win.render()
+
+}
+
+func Test_window_row_start_end_positions(t *testing.T) {
+	windowWidth := 4
+	windowRowPositions := window_row_start_end_positions(14, windowWidth)
+	/*  good answers:
+	position: [12 13]
+	position: [8 11]
+	position: [4 7]
+	position: [0 3]
+	*/
+
+	compare_int_pair("rowStartEndPos1", windowRowPositions[0][0], 12, t)
+	compare_int_pair("rowStartEndPos2", windowRowPositions[0][1], 13, t)
+	compare_int_pair("rowStartEndPos3", windowRowPositions[1][0], 8, t)
+	compare_int_pair("rowStartEndPos4", windowRowPositions[1][1], 11, t)
+	compare_int_pair("rowStartEndPos5", windowRowPositions[2][0], 4, t)
+	compare_int_pair("rowStartEndPos6", windowRowPositions[2][1], 7, t)
+	compare_int_pair("rowStartEndPos7", windowRowPositions[3][0], 0, t)
+	compare_int_pair("rowStartEndPos8", windowRowPositions[3][1], 3, t)
 }
 
 func compare_int_pair(callerInfo string, received, wanted int, t *testing.T) {
