cfd6ca958203c3c3a10578857e623d1b9782b633
Balazs Nyiro <diogenesz@pergamen.hu>
2023-10-09 16:40:14 -0400

    user_interface_empty

---
+++ iris/iris.go
@@ -0,0 +1,30 @@
+/*
+author: Balazs Nyiro, balazs.nyiro.ca@gmail.com
+
+Copyright (c) 2023, Balazs Nyiro
+All rights reserved.
+
+This source code (all file in this repo) is licensed
+under the Apache-2 style license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
+package iris
+
+func UserInterfaceStart(ch_data_input chan MessageAndCharactersForWindowsUpdate, dataInputLineSeparator string) {
+	userInterfaceInit()
+	// ch_user_input := make(chan string)
+	// ch_terminal_size_change_detect := make(chan [2]int)
+
+}
+
+func userInterfaceInit() {
+	terminal_console_clear()
+	terminal_console_input_buffering_disable()
+	terminal_console_character_hide()
+}
+
+func UserInterfaceExit() {
+	terminal_console_character_show()
+	terminal_console_input_buffering_enable()
+}
